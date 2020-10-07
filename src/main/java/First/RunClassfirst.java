e499e15ca69c874cee838dfa2a6904e3cdeb94e8
DSampa <69985686+DSampa@users.noreply.github.com>
2020-10-07 19:53:32 -0300

    MyFirstApp_On_GitHub

---
+++ src/main/java/First/RunClassfirst.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package First;
+
+/**
+ *
+ * @author ID
+ */
+public class RunClassfirst {
+    public static void main(String[] args) {
+      FirstC  n = new FirstC();
+      FirstC ida = new FirstC();
+      FirstC b = new FirstC();
+      n.nome="Fulano";
+      ida.idade = 25;
+      b.bairro = "Bairro dos Desconhecidos";
+      
+        System.out.println("Nome: " +n.nome);
+        System.out.println("Idade: "+ida.idade);
+        System.out.println("Bairro: "+b.bairro);
+    }
+}
